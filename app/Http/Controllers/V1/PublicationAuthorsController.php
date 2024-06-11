e63b5ff444a77abf8f60537524a48070be9c8239
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-06-11 01:36:57 -0700

    feat: add PublicationAuthorsController for managing authors of a publication

---
+++ app/Http/Controllers/V1/PublicationAuthorsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Models\Publication;
+use Illuminate\Http\Request;
+
+/**
+ * @tags Authors
+ */
+class PublicationAuthorsController extends Controller
+{
+    /**
+     * Get all authors for a publication
+     *
+     * This endpoint allows you to get all authors for a publication.
+     *
+     * @return Author[] The authors for the publication
+     */
+    public function index(Publication $publication)
+    {
+        return $publication->authors;
+    }
+
+    /**
+     * Create a new author for a publication
+     *
+     * This endpoint allows you to create a new author for a publication.
+     *
+     * @return Author The newly created author
+     */
+    public function store(Publication $publication, Request $request)
+    {
+        $validated = $request->validate([
+            'primer_apellido' => 'required|string',
+            'segundo_apellido' => 'required|string',
+            'orc_id' => 'required|string|size:16',
+        ]);
+
+        $author = $publication->authors()->create($validated);
+
+        return response()->json($author, 201);
+    }
+}
---
+++ tests/Feature/Http/Controllers/V1/PublicationAuthorsControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Publication;
+use App\Models\User;
+
+it('allows to list authors of a publication', function () {
+    $user = User::factory()->create();
+    $publication = $user->publications()->create(Publication::factory()->make()->toArray());
+
+    $response = $this->actingAs($user)->get("api/v1/publications/{$publication->id}/authors");
+
+    $response->assertStatus(200);
+    $response->assertJson([]);
+});
+
+it('allows authors to be added to a publication', function () {
+    $user = User::factory()->create();
+    $publication = $user->publications()->create(Publication::factory()->make()->toArray());
+
+    $response = $this->actingAs($user)->post("api/v1/publications/{$publication->id}/authors", [
+        'primer_apellido' => 'Doe',
+        'segundo_apellido' => 'Smith',
+        'orc_id' => '0000000000000000',
+    ]);
+
+    $response->assertStatus(201);
+    $response->assertJson([
+        'primer_apellido' => 'Doe',
+        'segundo_apellido' => 'Smith',
+        'orc_id' => '0000000000000000',
+    ]);
+});
