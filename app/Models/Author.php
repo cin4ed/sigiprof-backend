633fd5f096f8fd8999177ee68f59ff0dbaf97f0e
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-06-11 01:35:05 -0700

    feat: add Author model

---
+++ app/Models/Author.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Author extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'autores';
+
+    protected $fillable = [
+        'primer_apellido',
+        'segundo_apellido',
+        'orc_id',
+    ];
+
+    /**
+     * Get the publications for the author.
+     */
+    public function publications()
+    {
+        return $this->belongsToMany(Publication::class, 'autores_publicaciones', 'autor_id', 'publicacion_id')
+            ->withTimestamps();
+    }
+}
