4c41bf726400b544a432ed9c3fe0dfce7312e4c6
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-06-10 22:18:29 -0700

    chore: Remove deprecated API route for user publications

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api/v1.php',
-        // apiPrefix: 'api/v1',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
