1039ccf8585645856f73c359fa9f73243f747a83
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-06-11 12:37:38 -0700

    chore: Refactor API routes for user publications, books, and authors

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -8,34 +8,17 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'index']);
-    Route::post('/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'store']);
+    Route::get(     '/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'index']);
+    Route::post(    '/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'store']);
 
-    Route::get('/publications', [\App\Http\Controllers\V1\PublicationController::class, 'index']);
-    Route::get('/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'show']);
-    Route::put('/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'update']);
-    Route::delete('/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'destroy']);
-    Route::get('publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'index']);
-    Route::post('publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'store']);
+    Route::get(     '/publications', [\App\Http\Controllers\V1\PublicationController::class, 'index']);
+    Route::get(     '/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'show']);
+    Route::put(     '/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'update']);
+    Route::delete(  '/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'destroy']);
+    
+    Route::get(     '/publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'index']);
+    Route::post(    '/publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'store']);
+
+    Route::get(     '/user/books', [\App\Http\Controllers\V1\UserBooksController::class, 'index']);
+    Route::post(    '/user/books', [\App\Http\Controllers\V1\UserBooksController::class, 'store']);
 });
-
-// /login
-// /register
-// /logout
-
-// /v1/user/publications |GET|POST|
-// /v1/user/publications/:id |GET|PUT|DELETE|
-// /v1/user/publications/:id/upload |POST|
-// /v1/user/publications/:id/download |GET|
-// /v1/user/publications/:id/participants |GET|POST|
-// /v1/user/publications/:id/authors |GET|POST|
-
-// /v1/user/books |GET|POST|
-// /v1/user/books /:id |GET|PUT|DELETE|
-// /v1/user/books /:id/upload |POST|
-// /v1/user/books /:id/download |GET|
-
-// /v1/user/courses |GET|POST|
-// /v1/user/courses/:id |GET|PUT|DELETE|
-// /v1/user/courses/:id/upload |POST|
-// /v1/user/courses/:id/download |GET|
