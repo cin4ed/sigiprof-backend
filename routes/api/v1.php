98eeca51be6b71dde02487fc0feaf55b535bcf38
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-06-11 16:39:16 -0700

    chore: add comments to dive endpoints

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -8,6 +8,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    // Publications
+
     Route::get('/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'index']);
     Route::post('/user/publications', [\App\Http\Controllers\V1\UserPublicationsController::class, 'store']);
 
@@ -16,8 +19,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'update']);
     Route::delete('/publications/{publication}', [\App\Http\Controllers\V1\PublicationController::class, 'destroy']);
 
+    // Authors
+
     Route::get('/publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'index']);
     Route::post('/publications/{publication}/authors', [\App\Http\Controllers\V1\PublicationAuthorsController::class, 'store']);
+
+    // Books
 
     Route::get('/user/books', [\App\Http\Controllers\V1\UserBooksController::class, 'index']);
     Route::post('/user/books', [\App\Http\Controllers\V1\UserBooksController::class, 'store']);
